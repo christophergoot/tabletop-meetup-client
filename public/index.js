1ca67190e2b0bae52dd0e6e9b360da48233b67f9
christophergoot <christophergoot@gmail.com>
2018-04-27 08:46:47 -0700

    basic wireframes

--- public/index.js
+++ public/index.js
@@ -1,12 +1,10 @@
-// import MOCK_DATA from './mock-data';
-
 function loadGameList(collection) {
 	const list = document.querySelector(".game-list");
 	const cards = collection.map(game => {
-		if (game.owned) {
-			const { name, thumbnail, yearPublished, averageRating, minPlayers, maxPlayers, playingTime} = game;
+		if (game.owned && !game.isExpansion) {
+			const { name, image, thumbnail, yearPublished, averageRating, minPlayers, maxPlayers, playingTime} = game;
 			const card = 
-				`<div class='game-card'>
+				`<div class='game-card' style='background-image: url(${thumbnail})'>
 					<h3><a href>${name}</a> (${yearPublished})</h3>
 					<span>${averageRating.toFixed(2)} Average Rating</span>
 					<span>${minPlayers} to ${maxPlayers} Players</span>
