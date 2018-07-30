3f86759a51e8b4d4811d9e430d195be1be9d1f08
christophergoot <christophergoot@gmail.com>
2018-07-30 12:03:41 -0700

    api url in config.js

--- src/config.js
+++ src/config.js
@@ -3,15 +3,4 @@
 export const STATIC_MEDIA_FOLDER = 'http://tabletop-meetup.netlify.com/media/';
 // export const STATIC_MEDIA_FOLDER = 'http://localhost:3000/media/';
 
-console.log(process.env);
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://tabletop-meetup-api.herokuapp.com/api/';
-
-
-// let backendHost;
-// const hostname = window && window.location && window.location.hostname;
-// if(hostname === 'tabletop-meetup.netlify.com') {
-// 	backendHost = 'https://tabletop-meetup-api.herokuapp.com/';
-// } else {
-// 	backendHost = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3030';
-// }
-// export const API_BASE_URL = `${backendHost}/api/`;
