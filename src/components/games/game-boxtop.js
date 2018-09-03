2c643659c69ebec668d4bf5709af2d235a0c97a7
christophergoot <christophergoot@gmail.com>
2018-09-03 10:21:07 -0700

    responsive styling

--- src/components/games/game-boxtop.js
+++ src/components/games/game-boxtop.js
@@ -1,13 +1,11 @@
 import React from 'react';
-// import CollectionDetails from './collection-details';
 import { STATIC_MEDIA_FOLDER } from '../../config';
-
 import './game-boxtop.css';
+
 
 export default function GameBoxtop(props) {
 	const { gameId, name, thumbnail, yearPublished, averageRating, 
 		minPlayers, maxPlayers, playingTime, rank } = props.game;
-	// const { owners } = props;
 	const externalLink = `https://boardgamegeek.com/boardgame/${gameId}/`;
 	const playerCount = (minPlayers, maxPlayers) => {
 		if (minPlayers === maxPlayers) {
@@ -15,16 +13,6 @@ export default function GameBoxtop(props) {
 		}
 		else return (`${minPlayers} - ${maxPlayers}`);
 	};
-
-	// let ownerReport = '';
-	// if (owners && owners.length === 1) ownerReport = <span>Owned by {owners}</span>;
-	// else if (owners && owners.length > 1) {
-	// 	ownerReport = 'Owned by ';
-	// 	for(let i=0; i<owners.length-1; i++) {
-	// 		ownerReport += `${owners[i]} & `;
-	// 	}
-	// 	ownerReport += owners[owners.length-1];
-	// }
 
 	let yearReport = '';
 	if (yearPublished > 0) yearReport = `(${yearPublished})`;
@@ -70,29 +58,6 @@ export default function GameBoxtop(props) {
 					<p>{playingTime}</p>
 				</div>
 			</div>
-			{/* <div className='game-owners'>{ownerReport}</div> */}
 		</div>
-
 	);
-
-	// return (
-	// 	<div style={{display:'flex'}}>
-	// 		<div style={{flexDirection:'column'}} >
-	// 			<img src={thumbnail} style={{height:'4em',border:'1px solid black',boxShadow:'1px 2px 3px gray',display:'block'}}/>
-	// 			{props.listManager}
-	// 		</div>
-	// 		<div>
-	// 			<h3><a href={externalLink} target='_blank'>{name}</a> {yearReport}</h3>
-	// 			{rankReport}
-	// 			<div className='boxtop-deets'>
-	// 				<span>{averageRating.toFixed(2)} Average Rating</span>
-	// 				<span>{playerCount(minPlayers, maxPlayers)}</span>
-	// 				<span>{playingTime} Min Playing Time</span>
-	// 			</div>
-	// 			{ownerReport}
-	// 		</div>
-	// 	</div>
-	// );
-
-
 }
